81d32eaab0136b3466bafdb69382182b1b488468
Sangin-C <sangin3435@naver.com>
2021-07-16 14:59:05 +0900

    블로그 4번까지 완료

--- src/main/java/com/example/batchtest/job/BatchConfig.java
+++ src/main/java/com/example/batchtest/job/BatchConfig.java
@@ -5,8 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,17 +22,20 @@ public class BatchConfig {
     @Bean
     public Job testJob() {
         return jobBuilderFactory.get("testJob")
-                .start(testStep())
+                .start(testStep(null))
                 .build();
     }
 
     @Bean
-    public Step testStep() {
+    @JobScope
+    public Step testStep(@Value("#{jobParameters[testParameter]}") String testParameter ) {
         return stepBuilderFactory.get("testStep")
                 .tasklet((contribution, chunkContext) -> {
                     log.info("=====================testStep 실행===============");
+                    log.info("=====================testParameter : "+testParameter+"===============");
                     return RepeatStatus.FINISHED;
                 })
                 .build();
     }
 }
+
